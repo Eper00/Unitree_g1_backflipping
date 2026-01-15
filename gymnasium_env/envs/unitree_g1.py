af114077821bf97dbdb9299d628a9be35ba48905
Epres Tamás <104941550+Eper00@users.noreply.github.com>
2026-01-15 22:14:01 +0100

    Add files via upload

--- gymnasium_env/envs/unitree_g1.py
+++ gymnasium_env/envs/unitree_g1.py
@@ -1,17 +1,9 @@
-from enum import Enum
 import gymnasium as gym
-from gymnasium import spaces
-import pygame
 import numpy as np
-from mujoco import MjModel,MjData
-from mujoco import viewer
-
-
-
+from mujoco import MjModel,MjData,viewer,mj_step
+import time
 class UnitreeG1Env(gym.Env):
- 
     def __init__(self, render_mode=None):
-        # Mujoco model betöltése
         self.model = MjModel.from_xml_path(
             "/home/tomi/AIMotionLab-Virtual/scripts/unitree_g1/mujoco_menagerie/unitree_g1/scene_mjx.xml"
         )
@@ -21,47 +13,116 @@ class UnitreeG1Env(gym.Env):
         self.qpos = self.data.qpos
         self.qvel = self.data.qvel
         self.ctrl = self.data.ctrl
+        self.dim_qpos = len(self.qpos)
+        self.dim_qvel = len(self.qpos)
+        self.dim_ctrl = self.model.nu
         self.initial_qpos = self.qpos.copy()
         self.initial_qvel = self.qvel.copy()
+        self.target_qpos = self.qpos.copy()
+        self.target_qvel = self.qvel.copy()
 
-       
-        self.action_space = gym.spaces.Box(-0.4,0.4, (self.model.nu,) ,dtype=np.float32)
-        self.observation_space=gym.spaces.Box(-np.inf,np.inf, (len(self.qpos)*len(self.qvel),) ,dtype=np.float32)
+        actuator_joints = self.model.actuator_trnid[:, 0]
 
-     
+        low = []
+        high = []
+        self.ctrl_history=[]
+        self.ctrl_history.append(self.data.ctrl)
+        for j in actuator_joints:
+            low.append(self.model.jnt_range[j, 0])
+            high.append(self.model.jnt_range[j, 1])
+
+        low = np.array(low, dtype=np.float32)
+        high = np.array(high, dtype=np.float32)
+        self.action_space = gym.spaces.Box(
+            low=low,
+            high=high,
+            dtype=np.float32
+        )   
+        self.observation_space=gym.spaces.Box(-np.inf,np.inf, (len(self.qpos) + len(self.qvel) + len(self.ctrl),) ,dtype=np.float32)
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self._target_location}
+        return np.concatenate([self.qpos, self.qvel,self.ctrl]).astype(np.float32)
+
 
     def _get_info(self):
         return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
+            "distance_qpos": np.linalg.norm(
+                self.qpos-self.target_qpos, ord=1
+            ),
+            "distance_qvel": np.linalg.norm(
+                self.qvel-self.target_qvel, ord=1
             )
         }
+    def _get_reward(self):
+        reward_qpos = np.linalg.norm(self.qpos - self.target_qpos)
+        reward_qvel = np.linalg.norm(self.qvel - self.target_qvel)
+        return -(reward_qpos+0)
 
     def reset(self, seed=None, options=None):
-      
-        pass
+        super().reset(seed=seed)
 
+        self.data.qpos[:] = self.target_qpos
+        self.data.qvel[:] = self.target_qvel
+        self.data.ctrl[:] = 0.0
+
+        observation = self._get_obs()
+        info = self._get_info()
+        return observation, info
     def step(self, action):
-     
+        action = np.asarray(action, dtype=np.float32)
 
-        return observation, reward, terminated, False, info
+        # Apply action
+        self.data.ctrl[:] = action
+        self.ctrl_history.append(self.data.ctrl.copy())  # copy kell, külön tároljuk minden step-et
+
+        # MuJoCo léptetés
+        mj_step(self.model, self.data)
+
+        # Observation, reward, info
+        obs = self._get_obs()
+        reward = self._get_reward()
+        info = self._get_info()
+
+        base_z = self.data.qpos[2]
+        terminated = base_z < 0.25
+
+        return obs, reward, terminated, False, info
+
+
 
     def render(self):
-        # Csak human mód
-        if not hasattr(self, "_viewer"):
-            # Egyszeri ablaknyitás
-            self._viewer = viewer.launch(self.model, self.data)
-        else:
-            # Frissítjük a jelenlegi frame-et
-            self._viewer.render()
+        """
+        Replay render: végigmegyünk a ctrl_history-n,
+        minden lépésnél frissítjük a robotot a history-ból.
+        """
+        # Start time
+        start = time.time()
+        self.reset()  # Reseteljük a robotot
+        self.viewer = viewer.launch_passive(self.model, self.data) 
+        
 
-    def _render_frame(self):
-      
+        i = 0
+        while  self.viewer.is_running():
+            if (i>=len(self.ctrl_history)):
+                i=len(self.ctrl_history)-1
+            # Frissítjük a controlt
+            self.ctrl[:] = self.ctrl_history[i]
 
-      pass
+            # MuJoCo léptetés
+            mj_step(self.model, self.data)
+
+            # Viewer frissítés
+            self.viewer.sync()
+
+            # Lassítás a vizualizációhoz
+
+            i += 1
+        self.reset()
+   
+
+
+
+        
 
     def close(self):
       pass
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import gymnasium as gym
 import gymnasium_env
-env = gym.make('gymnasium_env/UnitreeG1-v0', render_mode='human')
-env.reset()
+import numpy as nps
+import numpy as np
+
+
+
+env = gym.make('gymnasium_env/UnitreeG1-v0')
+obs, info = env.reset()
+
+for i in range (1000):
+    action = np.ones(env.unwrapped.model.nu) * i/1000
+    obs, reward, terminated, truncated, info = env.step(action)
+
 env.render()
